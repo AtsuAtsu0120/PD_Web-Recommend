f7ce1bf00d41b258e6091db2879a781ff98486d5
shunsuke <calt.nkt.2575@gmail.com>
2024-01-05 20:47:14 +0900

    HeroSectionの記述しましたpart2
    ExploreとResultの二つあって色だけ分かれてます

---
+++ Frontend/ui/HeroSectionResult.tsx
@@ -0,0 +1,27 @@
+//ヒーローセクション.result
+import {Center,Box,VStack } from "@yamada-ui/react";
+
+export function HeroSectinon() {
+    return(
+        <div>
+            
+            <Box position="relative"boxSize="2xs"text="40"fontWeight="550"fontStyle="oblique"bg="primary"p="md"color="#141414"_after={{content: "'ASA'",position: "absolute", top: "5", left: "15",bg: "secondary",color: "#474747",}}>
+
+            </Box>
+
+            <Center h="300" rounded="md" bg="#FFFFFF">
+            <VStack>
+
+                <Center text="32" fontWeight="800" rounded="md" color="#474747">
+                 Result
+                </Center>
+                <Center text="15" rounded="md" color="#212121">
+                 見つかったサイト
+                </Center>
+
+            </VStack>
+            </Center>
+              
+        </div>
+    );
+  }
